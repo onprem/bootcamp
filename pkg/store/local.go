09d53fa8820de0f0720a815c1bb128ca93676b44
Prem Saraswat <prmsrswt@gmail.com>
2021-06-13 22:45:02 +0530

    Fix persistence in local store

--- pkg/store/local.go
+++ pkg/store/local.go
@@ -2,44 +2,58 @@ package store
 
 import (
 	"encoding/gob"
+	"io"
 	"os"
 )
 
 type LocalStore struct {
 	db   map[string]string
-	file *os.File
+	path string
 }
 
 func NewLocal(path string) (*LocalStore, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		file, err = os.Create(path)
-		if err != nil {
-			return nil, err
-		}
-	}
-
 	ls := &LocalStore{
 		db:   make(map[string]string),
-		file: file,
+		path: path,
 	}
 
-	err = gob.NewDecoder(file).Decode(&ls.db)
-	if err != nil {
-		ls.db = make(map[string]string)
-	}
-
-	return ls, nil
+	return ls, ls.load()
 }
 
 func (s *LocalStore) Set(key string, value string) {
 	s.db[key] = value
-
-	_ = s.file.Truncate(0)
-	_ = gob.NewEncoder(s.file).Encode(s.db)
-	_ = s.file.Sync()
+	s.save()
 }
 
 func (s *LocalStore) Get(key string) string {
 	return s.db[key]
+}
+
+func (s *LocalStore) save() error {
+	f, err := os.Create(s.path)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	return gob.NewEncoder(f).Encode(s.db)
+}
+
+func (s *LocalStore) load() error {
+	f, err := os.Open(s.path)
+	if err != nil {
+		// File does not exists, skip returning error.
+		if os.IsNotExist(err) {
+			return nil
+		}
+		return err
+	}
+	defer f.Close()
+
+	err = gob.NewDecoder(f).Decode(&s.db)
+	if err == io.EOF {
+		s.db = make(map[string]string)
+		err = nil
+	}
+	return err
 }
